68a23c1135964cc0fa59566522acf4c0ad556ae3
Nicolas <nicolas.koelmel@icloud.com>
2018-09-26 09:54:42 +0200

    feathers rest for react app build

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,13 +1,13 @@
 import { restClient, authClient } from 'aor-feathers-client';
-import feathers from '@feathersjs/feathers'
-import rest from '@feathersjs/rest-client';
-import auth from '@feathersjs/authentication-client';
+import feathers, { rest, authentication } from '@feathersjs/client'
+// import rest from '@feathersjs/rest-client';
+// import auth from '@feathersjs/authentication-client';
 import $ from 'jquery'
 
 const feathersClient = feathers();
 const feathersRestClient = rest(process.env.REACT_APP_BACKEND_URI);
 feathersClient.configure(feathersRestClient.jquery($))
-feathersClient.configure(auth({
+feathersClient.configure(authentication({
   storage: window.localStorage
 }))
 // to rename id field for *all* resources use this syntax:
