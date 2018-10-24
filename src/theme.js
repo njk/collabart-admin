b2e563ac38b6f3fcc50b7cf9572ffddc97b00d24
Nicolas <nicolas.koelmel@icloud.com>
2018-10-24 20:49:57 +0200

    removed unused

--- src/theme.js
+++ src/theme.js
@@ -1,5 +1,4 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import blueGrey from '@material-ui/core/colors/blueGrey';
 import grey from '@material-ui/core/colors/grey';
 import red from '@material-ui/core/colors/red';
 
